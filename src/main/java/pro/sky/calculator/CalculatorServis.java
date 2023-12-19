0939556091163f99837406b04886bf7caa6f2892
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2023-12-19 18:32:20 +0300

    calculator

--- src/main/java/pro/sky/calculator/CalculatorServis.java
+++ src/main/java/pro/sky/calculator/CalculatorServis.java
@@ -3,17 +3,19 @@ package pro.sky.calculator;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalculatorServis implements CalculaytorServiceInterface {
+public class CalculatorServis implements CalculaytorServicelmpl {
     @Override
     public String calculator() {
         return "Добро пожаловать в калькулятор";
     }
+
     @Override
     public int plus(int num1, int num2) {
         return num1 + num2;
     }
+
     @Override
-    public  int minus(int num1, int num2) {
+    public int minus(int num1, int num2) {
         return num1 - num2;
     }
 
